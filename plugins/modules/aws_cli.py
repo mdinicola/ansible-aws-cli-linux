9ab09151532fbb57bb4f85b7e7d2b0549a57a74f
Mike Di Nicola <mike@mdinicola.com>
2024-03-17 14:20:10 -0400

    fill in module details

--- plugins/modules/aws_cli.py
+++ plugins/modules/aws_cli.py
@@ -9,74 +9,79 @@ DOCUMENTATION = r'''
 ---
 module: my_test
 
-short_description: This is my test module
+short_description: Provisions AWS CLI in Linux
 
-# If this is part of a collection, you need to use semantic versioning,
-# i.e. the version is of the form "2.5.0" and not "2.4".
 version_added: "1.0.0"
 
-description: This is my longer description explaining my test module.
+description: This module installs, updates, or uninstalls the AWS CLI tool in Linux
 
 options:
-    name:
-        description: This is the message to send to the test module.
-        required: true
+    state:
+        description: 
+            - Indicates the desired tool state.
+            - Use O(update) to update to a new version if one is available
+        choices: ['present','absent','update']
+        default: 'present'
+        required: no
         type: str
-    new:
+    download_dir:
         description:
-            - Control to demo if the result of this module is changed or not.
-            - Parameter description can be a list as well.
-        required: false
-        type: bool
-# Specify this value according to your collection
-# in format of namespace.collection.doc_fragment_name
-# extends_documentation_fragment:
-#     - my_namespace.my_collection.my_doc_fragment_name
+            - Specify where to download the AWS CLI install script.
+            - Defaults to a temporary directory.
+        type: str
+    bin_dir: 
+        description: 
+            - Override the default location of where I(aws) is installed
+        default: '/usr/local/bin'
+        required: no
+        type: str
+    install_dir:
+        description: 
+            - Override the default location of where I(aws) files are copied
+        default: '/usr/local/aws-cli'
+        required: no
+        type: str
 
 author:
     - Mike Di Nicola (@mdinicola)
 '''
 
 EXAMPLES = r'''
-# Pass in a message
-- name: Test with a message
-  my_namespace.my_collection.my_test:
-    name: hello world
+# Install the AWS CLI
+- name: Install AWS CLI
+  mdinicola.aws_tools_linux.aws_cli:
+    state: present
 
-# pass in a message and have changed true
-- name: Test with a message and changed output
-  my_namespace.my_collection.my_test:
-    name: hello world
-    new: true
+# Update the AWS CLI to a newer version
+- name: Update AWS CLI
+  mdinicola.aws_tools_linux.aws_cli:
+    state: update
 
 # fail the module
-- name: Test failure of the module
-  my_namespace.my_collection.my_test:
-    name: fail me
+- name: Update AWS CLI
+  mdinicola.aws_tools_linux.aws_cli:
+    state: absent
 '''
 
 RETURN = r'''
-# These are examples of possible return values, and in general should use other names for return values.
-original_message:
-    description: The original name param that was passed in.
-    type: str
-    returned: always
-    sample: 'hello world'
+## These are examples of possible return values, and in general should use other names for return values.
 message:
     description: The output message that the test module generates.
     type: str
     returned: always
-    sample: 'goodbye'
+    sample: 'aws cli installed successfully'
 '''
 
 from ansible.module_utils.basic import AnsibleModule
-
+import os
 
 def run_module():
     # define available arguments/parameters a user can pass to the module
     module_args = dict(
-        name=dict(type='str', required=True),
-        new=dict(type='bool', required=False, default=False)
+        state=dict(type='str', required=False, default = 'present'),
+        download_dir=dict(type='str', required=False, default=None),
+        bin_dir=dict(type='str', required=False, default='/usr/local/bin'),
+        install_dir=dict(type='str', required=False, default='/usr/local/aws-cli')
     )
 
     # seed the result dict in the object
@@ -85,9 +90,7 @@ def run_module():
     # state will include any data that you want your module to pass back
     # for consumption, for example, in a subsequent task
     result = dict(
-        changed=False,
-        original_message='',
-        message=''
+        changed=False
     )
 
     # the AnsibleModule object will be our abstraction working with Ansible
@@ -107,19 +110,32 @@ def run_module():
 
     # manipulate or modify the state as needed (this is going to be the
     # part where your module will do what it needs to do)
-    result['original_message'] = module.params['name']
-    result['message'] = 'goodbye'
+    
+    if module.params['state'] == 'present':
+        if os.path.exists(os.path.join(module.params['bin_dir'], 'aws')):
+            result['message'] = 'aws cli already exists'
+            module.exit_json(**result)
 
+    elif module.params['state'] == 'absent':
+        if not os.path.exists(os.path.join(module.params['bin_dir'], 'aws')):
+            result['message'] = 'aws cli is not installed'
+            module.exit_json(**result)
+
+        
+
+    elif module.params['state'] == 'update':
+            pass
+    
     # use whatever logic you need to determine whether or not this module
     # made any modifications to your target
-    if module.params['new']:
-        result['changed'] = True
+    #if module.params['new']:
+    #    result['changed'] = True
 
     # during the execution of the module, if there is an exception or a
     # conditional state that effectively causes a failure, run
     # AnsibleModule.fail_json() to pass in the message and the result
-    if module.params['name'] == 'fail me':
-        module.fail_json(msg='You requested this to fail', **result)
+    #if module.params['name'] == 'fail me':
+    #    module.fail_json(msg='You requested this to fail', **result)
 
     # in the event of a successful module execution, you will want to
     # simple AnsibleModule.exit_json(), passing the key/value results
